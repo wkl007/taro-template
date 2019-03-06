19d592362a032e38ff24b6c0acc73fd8e8624cda
wkl--007 <499657357@qq.com>
2019-03-06 21:17:26 +0800

    update utils

---
+++ src/utils/taroUtils.js
@@ -0,0 +1,47 @@
+import Taro from '@tarojs/taro'
+
+export function taroLogin () {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+export function taroPay () {
+  return new Promise((resolve, rejcet) => {
+
+  })
+}
+
+export function taroDownload () {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+export function taroDownloadImage () {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+export function taroUploadFile () {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+export function taroUpdate () {
+
+}
+
+export function taroAddCard () {
+  return new Promise((resolve, reject) => {
+
+  })
+}
+
+export function taroGetSystemInfo () {
+  return new Promise((resolve, reject) => {
+
+  })
+}
